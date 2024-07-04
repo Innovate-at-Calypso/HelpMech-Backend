dc635c3c765d348fb1e7ca709c6d2f0a35202ccf
Pavan Gowda T S <pavangowdats01@gmail.com>
2024-07-04 07:15:18 +0530

    first commit of backend

---
+++ routes/user_routes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const User = require('../Models/user_model')
+authRoutes = express.Router();
+
+authRoutes.post("/app/signup", async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      const existingUser = await User.findOne({ email });
+  
+      if (existingUser) {
+        return res.status(400).json({ msg: "User with the same Email already exists!" });
+      }
+  
+      const hashedPassword = await bcryptjs.hash(password, 8);
+  
+      let user = new User({
+        email,
+        password: hashedPassword,
+        name,
+      });
+  
+      user = await user.save();
+      const token = jwt.sign({id : user._id},"PasswordKey");
+       res.json({token,...user._doc});
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
+
+
+  authRoutes.post("/app/login",async (req,res)=>{
+    try{
+      const {email ,password}=req.body;
+      const user = await User.findOne({email});
+ 
+      if(!user){
+        return res.status(400).json({msg: "User with this Email doesn't Exits !"});
+      }
+ 
+      const isMatch = await bcryptjs.compare(password,user.password);
+ 
+      if(!isMatch){
+        return res.status(400).json({msg : "Incorrect Password"});
+      }
+ 
+      const token = jwt.sign({id : user._id},"PasswordKey");
+      res.json({token,...user._doc});
+    }
+    catch(e){
+     res.status(500).json({ error : e.message});
+    }
+ });
+
+ authRoutes.get('/tokenIsVaild',async (req,res)=>{
+    try{
+       const token = req.body.token;
+       if(!token){
+         return res.status(400).json({msg : "Error In Token"});
+       }
+       const verified = jwt.verify(token,"PasswordKey");
+       if(!verified){
+         return res.status(400).json({msg : "Error In Verified"})
+       }
+       const user = await User.findById(verified.id);
+       if(!user) return res.status(400).json({msg : "Error In User"});
+       res.json({token,...user._doc});
+    }catch(e){
+       res.status(500).json({error : "Invalid Token"})
+    }
+ });
+
+module.exports = authRoutes;
